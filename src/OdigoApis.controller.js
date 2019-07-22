c44f6a30960df27d78c287f8bcc104f1f7cab1c1
raulnaldo@gmail.com <raulnaldo@gmail.com>
2019-07-22 13:18:10 +0200

    Nuevos metodos con QueryString

--- src/OdigoApis.controller.js
+++ src/OdigoApis.controller.js
@@ -1,19 +1,32 @@
+//http://localhost:3000/Apis.html#?agentId=agente176ddi@demo.com&gate=InfoGate&callref=98769dsfjsdfj9079087
 (function () {
 "use strict";
 
 angular.module('OdigoApisModule')
 .controller('OdigoApisController', OdigoApisController);
 
-OdigoApisController.$inject = ['OdigoApisService','userUid','appUid'];
-function OdigoApisController(OdigoApisService,userUid,appUid) {  
+OdigoApisController.$inject = ['$location','OdigoApisService','userUid','appUid'];
+function OdigoApisController($location,OdigoApisService,userUid,appUid) {  
+
+  console.log('--> OdigoApisController Init()');
+
   var OdigoApisCtrl = this;
+  OdigoApisCtrl.OdigoCallInfo=$location.search();
+
+  console.log('--> OdigoApisCtrl.OdigoCallInfo():',OdigoApisCtrl.OdigoCallInfo);
 
   OdigoApisCtrl.buttonSearchText='Get The Token Key!';
   OdigoApisCtrl.Token='';
   OdigoApisCtrl.userUid=userUid;
   OdigoApisCtrl.appUid=appUid;
-  OdigoApisCtrl.OpStatus='';
+  OdigoApisCtrl.userInfo={};
+  OdigoApisCtrl.userActiveSettings={};  
+  OdigoApisCtrl.OpStatus=''; 
 
+
+//********************************
+//ODIGO APIS METHODS
+//********************************
 
   //GET THE TOKEN KEY
   OdigoApisCtrl.getTokenFromApi = function(){
@@ -31,7 +44,41 @@ function OdigoApisController(OdigoApisService,userUid,appUid) {
   };
   
 
-  //ODIGO API COMANDS
+//ODIGO AGENT STATUS
+
+  OdigoApisCtrl.OdigoGetAgent = function(Token,Agent){
+    console.log("--OdigoApisCtrl.OdigoGetAgent():",Token,Agent);
+    OdigoApisCtrl.OpStatus='';
+      var promise= OdigoApisService.OdigoGetAgent(Token,Agent);
+      promise.then(function (response) {
+          OdigoApisCtrl.OpStatus='200';
+          OdigoApisCtrl.userActiveSettings={};
+          OdigoApisCtrl.userInfo=response.data;          
+        })
+        .catch(function (error) {
+          console.log("Error:",error.status);
+          OdigoApisCtrl.OpStatus=error.status;
+      });                
+  }; 
+
+  OdigoApisCtrl.OdigoGetAgentActiveSettings = function(Token,Agent){
+    console.log("--OdigoApisCtrl.OdigoGetAgentActiveSettings():",Token,Agent);
+    OdigoApisCtrl.OpStatus='';
+      var promise= OdigoApisService.OdigoGetAgentActiveSettings(Token,Agent);
+      promise.then(function (response) {
+          OdigoApisCtrl.OpStatus='200';
+          OdigoApisCtrl.userActiveSettings=response.data[0];
+          console.log('OdigoApisCtrl.userActiveSettings:',OdigoApisCtrl.userActiveSettings);                              
+        })
+        .catch(function (error) {
+          console.log("Error:",error.status);
+          OdigoApisCtrl.OpStatus=error.status;
+      });                
+  }; 
+
+
+
+//ODIGO API COMANDS
 
   //HANGUP  
   OdigoApisCtrl.OdigoHangUp = function(Token){
@@ -47,6 +94,31 @@ function OdigoApisController(OdigoApisService,userUid,appUid) {
       });                
   };    
 
+  //END WRAPUP
+  OdigoApisCtrl.OdigoEndWrapUp = function(Token,Agent){
+    OdigoApisCtrl.OpStatus='';
+    OdigoApisCtrl.CallReasonCreate={};
+    OdigoApisCtrl.CallReasonCreate.callId='';
+    OdigoApisCtrl.CallReasonCreate.gateId='';
+    OdigoApisCtrl.CallReasonCreate.keyboardDuration=0;
+    OdigoApisCtrl.CallReasonCreate.reason={};
+    OdigoApisCtrl.CallReasonCreate.reason.key='Folder';
+    OdigoApisCtrl.CallReasonCreate.reason.value='Valor De Reason';
+    OdigoApisCtrl.CallReasonCreate.reasons=[OdigoApisCtrl.CallReasonCreate.reason];
+    OdigoApisCtrl.CallReasonCreate.storing=true;
+    OdigoApisCtrl.CallReasonCreate.wrapUpEnd=true;    
+    
+
+      var promise= OdigoApisService.OdigoEndWrapUp(Token,Agent,OdigoApisCtrl.CallReasonCreate.reasons);
+      promise.then(function (response) {
+          OdigoApisCtrl.OpStatus='200';
+          console.log('Then:',response.data);          
+        })
+        .catch(function (error) {
+          console.log("Error:",error.status);
+          OdigoApisCtrl.OpStatus=error.status;
+      });                
+  };
 
   //ANSWER
   OdigoApisCtrl.OdigoAnswer = function(Token){
--- src/OdigoApis.service.js
+++ src/OdigoApis.service.js
@@ -9,6 +9,16 @@ OdigoApisService.$inject = ['$http', 'ApiPath','ApiAuthPath','userUid','appUid']
 function OdigoApisService($http, ApiPath,ApiAuthPath,userUid,appUid) {
   var service = this;
 
+
+
+//********************************
+//ODIGO APIS METHODS
+//********************************
+
+
+
+//GET THE TOKEN KEY
+//*****************************
   service.getToken = function () {
     var MyToken={};
     var response = $http({
@@ -24,8 +34,45 @@ function OdigoApisService($http, ApiPath,ApiAuthPath,userUid,appUid) {
     return response;
   };
 
-  service.OdigoHangUp = function (Token) {
-    console.log('Enviamos eso:',Token);
+
+
+//GET AGENT STATUS
+//*****************************
+  service.OdigoGetAgent = function (Token,Agent) {
+    console.log('Enviamos eso:',Token,Agent);
+    var response = $http({
+      method: "GET",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40'))
+    });
+    return response;
+  };
+
+  service.OdigoGetAgentActiveSettings = function (Token,Agent) {
+    console.log('Enviamos eso:',Token,Agent);
+    var response = $http({
+      method: "GET",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/active-settings')
+    });
+    return response;
+  };  
+
+
+//HANG UP
+//*****************************
+  service.OdigoHangUp = function (Token,Agent) {
+    console.log('--> OdigoHangUp()');
     var response = $http({
       method: "POST",
       headers: {
@@ -34,12 +81,146 @@ function OdigoApisService($http, ApiPath,ApiAuthPath,userUid,appUid) {
          'X-API-TOKEN' : Token,
          'X-WS-INSTANCE' : 'de01'
        },       
-      url: (ApiPath + 'agent176ddi%40demo.com' + '/commands/hangUpCall')
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/hangUpCall')
     });
-
+    console.log('<-- OdigoHangUp()');
     return response;
   };
-  
+
+//HOLD CALL
+//*****************************
+  service.OdigoHold = function (Token,Agent) {
+    console.log('--> OdigoHold()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/holdOnCall')
+    });
+    console.log('<-- OdigoHold()');
+    return response;
+  };
+
+//OUTGOING CALL
+//*****************************
+  service.OdigoOutGoingCall = function (Token,Agent,OutgoingCall) {
+    console.log('--> OdigoOutGoingCall()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/holdOnCall'),
+      data: OutgoingCall
+    });
+    console.log('<-- OdigoOutGoingCall()');
+    return response;
+  };
+
+//RETREAVE
+//*****************************
+  service.OdigoOutRetreave = function (Token,Agent) {
+    console.log('--> OdigoOutRetreave()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/takeBackCall')
+      
+    });
+    console.log('<-- OdigoOutRetreave()');
+    return response;
+  };
+
+
+//TRANSFER
+//*****************************
+  service.OdigoTransfer = function (Token,Agent,TransferRequest) {
+    console.log('--> OdigoTransfer()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/takeBackCall'),
+      data: TransferRequest
+    });
+    console.log('<-- OdigoTransfer()');
+    return response;
+  };
+
+
+//SWITCH
+//*****************************
+  service.OdigoSwitch = function (Token,Agent) {
+    console.log('--> OdigoSwitch()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/comingAndGoingCall')
+    });
+    console.log('<-- OdigoSwitch()');
+    return response;
+  };
+
+//END WRAPUP
+//*****************************
+  service.OdigoEndWrapUp = function (Token,Agent,CallReasonCreate) {
+    console.log('--> OdigoEndWrapUp()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + Agent.replace('@', '%40') + '/commands/callFreeReason'),
+      data: CallReasonCreate
+    });
+    console.log('<-- OdigoEndWrapUp()');
+    return response;
+  };
+
+
+//CONFERENCE
+//*****************************
+  service.OdigoConference = function (Token) {
+    console.log('--> OdigoConference()');
+    var response = $http({
+      method: "POST",
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'Basic Y29uc29sZV9kZTAxQHByb3NvZGllLmNvbTpBWkVSVFk=',
+         'X-API-TOKEN' : Token,
+         'X-WS-INSTANCE' : 'de01'
+       },       
+      url: (ApiPath + 'agent176ddi%40demo.com' + '/commands/conferenceCall')
+    });
+    console.log('<-- OdigoConference()');    
+    return response;
+  };  
+
+
 }
 
 })();
