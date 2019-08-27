52b47e32fee0a298fe56f83b242514d214520f04
raulnaldo@gmail.com <raulnaldo@gmail.com>
2019-08-27 12:30:10 +0200

    Version Añadido start recording, stop recording, endwrapup

---
+++ CallScripterControl/OdigoControl.js
@@ -0,0 +1,160 @@
+<tr>
+    <td colspan="2" align="center">
+        <span id="csCon[attr]Field ID[/attr]" class="ctext" data-hidden="[attr]Hidden[/attr]">
+            <button class="btn cbutton" tabindex="[attr]Tab Order[/attr]" accesskey="[attr]Access Key[/attr]" style="    background-image: none;    background-color: #9B9B9B;    border-color: #9B9B9B;    box-shadow: 0px 2px 3px rgba(0,0,0,0.1);    -moz-box-shadow: 0px 2px 3px rgba(0,0,0,0.1);    -ms-box-shadow: 0px 2px 3px rgba(0,0,0,0.1);    text-shadow: 0 0px 0 #aaa;    color: #fff; border-radius: 6px !important;" id="fv[attr]Field ID[/attr]" type="button" name="fv[attr]Field ID[/attr]" onclick="ButtonClick_[attr]Control ID[/attr]_[attr]Version[/attr]([attr]Field ID[/attr], this, ('[attr]Validate Page[/attr]'.toLowerCase() == 'true'), '[attr encoding="js"]Command Name[/attr]', '[attr encoding="js"]Base API URL[/attr]', '[attr encoding="js"]AgentName[/attr]')">[attr]Text[/attr]</button>
+        </span>
+        <input type="hidden" name="F[attr]Field ID[/attr]" id="F[attr]Field ID[/attr]" onclick="$('#fv' + [attr]Field ID[/attr]).click();" value="[attr encoding="html"]Default Value[/attr]" >
+    </td>
+</tr>
+<script>
+<!--
+    var arguments[attr]Field ID[/attr] = '';
+    CS.Script.OnLoad(function() {
+        var arguments[attr]Field ID[/attr] = [attr]Arguments[/attr];
+        Load_[attr]Control ID[/attr]_[attr]Version[/attr]([attr]Field ID[/attr], ('[attr encoding="js"]Text[/attr]'.indexOf('[') > -1), '[attr encoding="jsnohtml"]Text[/attr]', '[attr]field property[/attr]', parseInt('[attr]Auto Click[/attr]',10), ('[attr]Disabled[/attr]'.toLowerCase() == 'true' ? true:false));
+    });
+// -->
+</script>
+
+<script global>
+function ButtonClick_[attr]Control ID[/attr]_[attr]Version[/attr](fieldId, btn, validatePage, Command, BaseURL, AgentName) {
+    Arguments = arguments + fieldId;
+    CS.Script.DoCalculations();
+    btn.disabled=true;
+    document.body.style.cursor = 'wait';
+    if (validatePage) {
+        if(CS.Script.Validate(true,false)) {
+            ExecCommand_[attr]Control ID[/attr]_[attr]Version[/attr](fieldId, btn, Command, Arguments, BaseURL, AgentName);
+        } else {
+            btn.disabled = false;
+            document.body.style.cursor = 'default';
+        } 
+    } else {
+        ExecCommand_[attr]Control ID[/attr]_[attr]Version[/attr](fieldId, btn, Command, Arguments, BaseURL, AgentName);
+    }
+}
+
+
+function ExecCommand_[attr]Control ID[/attr]_[attr]Version[/attr](fieldId, btn, Command, Arguments, BaseURL, AgentName)
+{
+    CS.Script.DoCalculations();
+    var args = Arguments;
+    var arg1 = '';
+    var arg2 = '';
+    var arg3 = '';
+    var arg4 = '';
+
+    if(args.length>0)
+        arg1=ReplaceWithFieldAndVariableValues(args[0]);
+    
+    if(args.length>1)
+        arg2=ReplaceWithFieldAndVariableValues(args[1]);
+    
+    if(args.length>2)
+        arg3=ReplaceWithFieldAndVariableValues(args[2]);
+    
+    if(args.length>3)
+        arg4=ReplaceWithFieldAndVariableValues(args[3]);
+
+    try {       
+        if (Command=='callFreeReason'){         
+            $.ajax({            
+                //url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(AgentName)+'/commands/'+Command,
+                url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(document.theForm.var_Login.value)+'/commands/'+Command,
+                headers: {
+                    'X-API-TOKEN': document.theForm.var_token.value,
+                    'Content-Type':'application/json',
+                    'X-WS-INSTANCE':'de01'
+                },
+                data: JSON.stringify({
+                "agentId":document.theForm.var_Login.value,
+                "callId":document.theForm.var_CallRef.value,
+                "gateId":document.theForm.var_Gate.value,
+                "keyboardDuration":0,
+                "reasons":[],
+                "storing":true,
+                "wrapUpEnd":true
+                }),
+                dataType: "application/json",                
+                method: 'POST',
+                success: function(data){
+                  console.log('succes: '+data);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {  
+                    console.log('error: '+textStatus);  
+                }  
+              });            
+        }
+        else{
+            if (Command=='stopRecordCall'){
+                $.ajax({            
+                    //url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(AgentName)+'/commands/'+Command,
+                    url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(document.theForm.var_Login.value)+'/commands/'+Command,
+                    headers: {
+                        'X-API-TOKEN': document.theForm.var_token.value,
+                        'Content-Type':'application/json',
+                        'X-WS-INSTANCE':'de01'
+                    },
+                    data: JSON.stringify(
+                        {
+                             "isRecordingBySupervisor": false
+                        }
+                    ),
+                    dataType: "application/json",
+                    method: 'POST',
+                    success: function(data){
+                      console.log('succes: '+data);
+                    },
+                    error: function(XMLHttpRequest, textStatus, errorThrown) {  
+                        console.log('error: '+textStatus);  
+                    }  
+                  });                 
+            }
+            else{
+                $.ajax({            
+                    //url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(AgentName)+'/commands/'+Command,
+                    url: BaseURL + '/agent/v1/agents/'+encodeURIComponent(document.theForm.var_Login.value)+'/commands/'+Command,
+                    headers: {
+                        'X-API-TOKEN': document.theForm.var_token.value,
+                        'Content-Type':'application/json',
+                        'X-WS-INSTANCE':'de01'
+                    },
+                    method: 'POST',
+                    success: function(data){
+                      console.log('succes: '+data);
+                    },
+                    error: function(XMLHttpRequest, textStatus, errorThrown) {  
+                        console.log('error: '+textStatus);  
+                    }  
+                  });
+          }
+        }
+    
+        CS.Script.DoCalculations();
+    }
+    catch(e) {
+        console.log(e);
+        Script.Toast.Warning(Command + ' failed', e, 5000);
+    }
+    btn.disabled = false;
+    document.body.style.cursor = 'default';
+}
+
+function Load_[attr]Control ID[/attr]_[attr]Version[/attr](fieldId, textIsVariable, text, fieldProperty, autoClick, disabled)
+{
+    var el = $('#fv' + fieldId);
+    if(textIsVariable) {
+        el.html(ReplaceWithFieldAndVariableValues(text));
+    }
+    if (autoClick >= parseInt(0,10)) {
+        setTimeout(function() {
+            el.click();
+        }, autoClick * 1000);
+    }
+
+    var field = $('#F' + fieldId).get(0).name;
+    if(disabled) {
+        Script.Fields.Disable(field);
+    }
+}
+</script>
--- odigoConsoleJavaScript/OpenCallScripter.js
+++ odigoConsoleJavaScript/OpenCallScripter.js
@@ -1,24 +1,32 @@
 try {
-
+  console.log('##########176-OPEN-SCRIPT-SANTANDER############'); 
   console.log('-->> OPEN SCRIPT ACTION'); 
 
   console.log('-->> getAgentDDI()');  
   console.log(odigo.getAgentDDI()); 
+  console.log('<<-- getAgentDDI()');  
 
   console.log('-->> odigo.getVoiceTaskId()'); 
   console.log(odigo.getVoiceTaskId());  
-
+  console.log('<<-- odigo.getVoiceTaskId()'); 
 
   console.log('-->> getVoiceGateLabel()');  
   console.log(odigo.getVoiceGateLabel()); 
-
+  console.log('<<-- getVoiceGateLabel()');  
 
   console.log('-->> getCustomerDdi()'); 
   console.log(odigo.getCustomerDdi());  
+  console.log('<<-- getCustomerDdi()');   
 
   console.log('-->> getHideFolder()');  
   console.log(odigo.getHideFolder()); 
+  console.log('<<-- getHideFolder()');
 
+  var vDATA;    
+}
+catch(error) {
+  console.error(error);
+}
 try{
   console.log(' --> odigo.getCustomerDdi()');
   var vPHONE= odigo.getCustomerDdi();
@@ -28,9 +36,11 @@ catch(error) {
   console.error(error);
   console.log(' <-- odigo.getCustomerDdi()');
 }
+
 try{
   console.log(' --> odigo.getHideFolder()');
-  var vDATA = odigo.getHideFolder();
+  var vDATA = odigo.getHideFolder();  
+  vDATA='RAUL|ORTEGA|1262|MarCantabrico23|Paracuellos de Jarama|Madrid|01-11-1975|27-08-2019|IBAN00356548461575';
   console.log(' <-- odigo.getHideFolder():',vDATA);
 }
 catch(error) {
@@ -87,35 +97,73 @@ try{
   console.error(error);
   console.log(' <-- odigo.getAgentLogin()');  
 }
+try{
+  console.log(' --> odigo.getVoiceGateLabel()');  
+  var vGATE = odigo.getVoiceGateLabel();
+  console.log(' <-- odigo.getVoiceGateLabel():',vGATE);  
+}  
+catch(error) {
+  console.error(error);
+  console.log(' <-- odigo.getVoiceGateLabel()');  
+}
 
+try{
+  console.log(' --> odigo.getCustomerSelected()');  
+  var vOdigoCustomer=odigo.getCustomerSelected('voice');  
+  console.log(' <--> CustomerSelected:',vOdigoCustomer);  
+  if (typeof(vOdigoCustomer) != 'undefined' && vOdigoCustomer != null) {
+    console.log(' --> BuildingDataFromCustomer:');  
+    vDATA= vOdigoCustomer.firstName + '|' + vOdigoCustomer.lastName + '|' + vOdigoCustomer.id + '|' + vOdigoCustomer.addresses[0].street1 + '|' + vOdigoCustomer.addresses[0].city + '|' + vOdigoCustomer.addresses[0].country + '|' + vOdigoCustomer.birthday + '|27-08-2019|IBAN5575464661163546';
+    console.log(' --> BuildingDataFromCustomer:',vDATA);  
+    console.log(' <-- BuildingDataFromCustomer:');      
+  }
+  else{
+    console.log(' <--> CUSTOMER NOT LOADED ACTIVATING SEARCH');
+    console.log(' --> odigo.search()');  
+    var vSearch=odigo.search("voice", odigo.getFolderNumber());
+    console.log(' <-- odigo.search():',vSearch);  
+    console.log(' --> New odigo.getCustomerSelected()');  
+    vOdigoCustomer=odigo.getCustomerSelected('voice');  
+    console.log(' <-- New odigo.getCustomerSelected():',vOdigoCustomer);      
+    if (typeof(vOdigoCustomer) != 'undefined' && vOdigoCustomer != null) {
+      console.log(' --> BuildingDataFromCustomer:');  
+      vDATA= vOdigoCustomer.firstName + '|' + vOdigoCustomer.lastName + '|' + vOdigoCustomer.id + '|' + vOdigoCustomer.addresses[0].street1 + '|' + vOdigoCustomer.addresses[0].city + '|' + vOdigoCustomer.addresses[0].country + '|' + vOdigoCustomer.birthday + '|27-08-2019|IBAN5575464661163546';
+      console.log(' --> BuildingDataFromCustomer:',vDATA);  
+    }
+  }
+  console.log(' <-- odigo.getCustomerSelected():',vOdigoCustomer);    
+}  
+catch(error) {
+  console.error(error);
+  console.log(' <-- odigo.getCustomerSelected()');  
+}
 
-console.log(' -->BUILD URL()'); 
-var URL = 'https://capgemini.callscripter.com/executer/script.aspx?
-            function=start&
-            diallerCampaign=Santander World Elite Mastercard&
-            csUsername=Odigo&
-            csPassword=OdigoV5&
-            restartScript=true&
-            var_Phone='+vPHONE+'&
-            var_OdigoData='+vDATA+'&
-            var_CallRef='+vCALLREF+'&
-            var_AgentName='+vAGENTNAME+'&
-            var_AgentLastName='+vAGENLAST+'&
-            var_Login='+vLOGIN;
-console.log(' <--BUILD URL()['+ URL +']');  
+try{  
+  console.log(' -->BUILD URL()'); 
+  //var URL = 'https://capgemini.callscripter.com/executer/script.aspx?function=start&diallerCampaign=Santander World Elite Mastercard&csUsername=Odigo&csPassword=OdigoV5&restartScript=true&var_Phone='+vPHONE+'&var_OdigoData='+vDATA+'&var_CallRef='+vCALLREF+'&var_AgentName='+vAGENTNAME+'&var_AgentLastName='+vAGENLAST+'&var_Login='+vLOGIN+'&var_Gate='+vGATE;
 
-console.log(' -->SET LOAD FRAME EMPTY()'); 
-category.loadIframe('');
-console.log(' <--SET LOAD FRAME EMPTY()'); 
+  var URL = 'https://capgemini.callscripter.com/executer/script.aspx?function=start&runFromEditor=true&script=160&restartScript=true&var_Phone='+vPHONE+'&var_OdigoData='+vDATA+'&var_CallRef='+vCALLREF+'&var_AgentName='+vAGENTNAME+'&var_AgentLastName='+vAGENLAST+'&var_Login='+vLOGIN+'&var_Gate='+vGATE;
+  console.log(' <--BUILD URL()['+ URL +']');  
 
-console.log(' --> LOAD FRAME()'); 
-category.loadIframe(URL);
-console.log(' <-- LOAD FRAME()'); 
+  console.log(' -->SET LOAD FRAME EMPTY()'); 
+  category.loadIframe('');
+  console.log(' <--SET LOAD FRAME EMPTY()'); 
 
-console.log('<<-- OPEN SCRIPT ACTION'); 
+  console.log(' --> LOAD FRAME()'); 
+  category.loadIframe(URL);
+  console.log(' <-- LOAD FRAME()'); 
+
+  console.log('<<-- OPEN SCRIPT ACTION'); 
+
+  console.log('*******************************************'); 
+  console.log('category.activePage: ' +category.activePage);
+  console.log('category.activeThirdPartyApp.iframeSrc: ' +category.activeThirdPartyApp.iframeSrc);
+  console.log('category.iframeSrc: ' +category.iframeSrc);
+  console.log('##########176-OPEN-SCRIPT-SANTANDER############'); 
 
 }
 catch(error) {
   console.error(error);
   console.log('<<-- OPEN SCRIPT ACTION');   
+  console.log('##########176-OPEN-SCRIPT-SANTANDER############'); 
 }
